130ff4406ae6395ddf32ce72b4358b5b959dbf62
Exploriter <watson@hey.com>
2021-01-22 10:31:45 -0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Jason Hunter Watson</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Jason Hunter Watson is designer, developer, writer, and teacher. Made in Los Angeles, CA.">
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;1,100;1,200;1,300;1,400&display=swap" rel="stylesheet">
+	<style>
+		/* https://github.com/csstools/sanitize.css */
+		*,::after,::before{box-sizing:border-box}::after,::before{text-decoration:inherit;vertical-align:inherit}html{font-family:system-ui,-apple-system,'Segoe UI',Roboto,sans-serif;cursor:default;line-height:1.5;overflow-wrap:anywhere;-moz-tab-size:4;tab-size:4;-webkit-tap-highlight-color:transparent;-webkit-text-size-adjust:100%;padding:1em}body{margin:0}h1{font-size:2em;margin:.67em 0}dl dl,dl ol,dl ul,ol dl,ul dl{margin:0}hr{color:inherit;height:0}nav ol,nav ul{list-style:url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg'/%3E");padding:0}nav li::before{content:'\200B'}abbr[title]{text-decoration:underline;text-decoration:underline dotted}b,strong{font-weight:bolder}code,kbd,pre,samp{font-family:Menlo,Consolas,'Roboto Mono',monospace;font-size:1em}pre{overflow:auto}small{font-size:80%}iframe,img,input,select,textarea{height:auto;max-width:100%}audio,canvas,iframe,img,svg,video{vertical-align:middle}audio:not([controls]){display:none;height:0}iframe{border-style:none}svg:not([fill]){fill:currentColor}table{border-collapse:collapse;border-color:inherit;text-indent:0}button,input,select,textarea{background-color:transparent;border:1px solid WindowFrame;color:inherit;font:inherit;letter-spacing:inherit;padding:.25em .375em;margin:0}button{text-transform:none}[type=button i],[type=reset i],[type=submit i],button{-webkit-appearance:button}[type=color i],[type=range i]{border-width:0;padding:0}fieldset{border:1px solid #a0a0a0;padding:.35em .75em .625em}progress{vertical-align:baseline}select{-moz-appearance:none;-webkit-appearance:none;background:no-repeat right center/1em;border-radius:0;padding-right:1em;text-transform:none}select:not([multiple]):not([size]){background-image:url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='4'%3E%3Cpath d='M4 0h6L7 4'/%3E%3C/svg%3E")}textarea{resize:vertical}[type=search i]{-webkit-appearance:textfield;outline-offset:-2px}::-webkit-inner-spin-button,::-webkit-outer-spin-button{height:auto}::-webkit-input-placeholder{color:inherit;opacity:.54}::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}::-moz-focus-inner{border-style:none;padding:0}:-moz-focusring{outline:1px dotted ButtonText}:-moz-ui-invalid{box-shadow:none}dialog{background-color:#fff;border:solid;color:#000;height:-moz-fit-content;height:-webkit-fit-content;height:fit-content;left:0;margin:auto;padding:1em;position:absolute;right:0;width:-moz-fit-content;width:-webkit-fit-content;width:fit-content}dialog:not([open]){display:none}summary{display:list-item}[aria-busy=true i]{cursor:progress}[aria-controls]{cursor:pointer}[aria-disabled=true i],[disabled]{cursor:not-allowed}[aria-hidden=false i][hidden]{display:initial}[aria-hidden=false i][hidden]:not(:focus){clip:rect(0,0,0,0);position:absolute}
+	</style>
+	<link rel="stylesheet" href="css/style-min.css">
+</head>
+<body>
+	<header>
+		<img src="images/avatar.png" alt="avatar" width="225" height="225">
+		<h1>Jason Hunter Watson</h1>
+		<p class="tagline">Explo(r|it)er. Truth-teller. Reputable fabulist.</p>
+		<p class="loc">Made in Los Angeles, CA.</p>
+	</header>
+	<div class="divider"></div>
+	<article>
+		<p>A <strong>generalist</strong> by nature—I do my best work when exploring complex ideas, simplifying complicated systems, and consciously leading teams in such endeavors with humor and candor.</p>
+		<p>I have worked as a software designer, web developer, project manager, product strategist, information architect, writer, fitness trainer, line cook, theatre projectionist, rare coin and diamond merchant, horticulturalist, glassblower, drug dealer, and postal worker.</p>
+		<p class="point">In 2009, I created a <a href="https://www.amazon.com/dp/1629145920" title="Healthy Green Drink" target="_blank">recipe blog</a> (defunct) to teach myself <abbr title="Search Engine Optimization">SEO</abbr> that landed me a multi-book deal which has since sold over 300,000 copies.</p>
+		<p class="point">From 2010–2015, I designed and developed over 200 websites for small to medium size businesses under the Designpx brand.</p>
+		<p class="point">From 2014–2020, I was part of the core leadership team at <a href="https://gmb.io" title="GMB Fitness" target="_blank">GMB Fitness</a> where I lead product development, implemented project management, and improved team cohesion systems and practices across the board.</p>
+		<div class="focus">
+			<p>My current focus is building the <a href="https://exploriter.com" title="Exploriter" target="_blank">Exploriter</a> brand which includes &hellip;</p>
+			<ul>
+				<li><strong><a href="https://expdaily.com" title="Minimum Daily Practice" target="_blank">exp/daily</a></strong> — a web app that encourages and tracks <em>minimum daily practice</em> in four transformative areas (reading, writing, movement, and meditation).</li>
+				<li><strong><a href="https://expcreator.com" title="exp/creator" target="_blank">exp/creator</a></strong> — a consultancy service which helps creators design, operate, and manage the systems and technology that delivers their brands and products.</li>
+			</ul>
+		</div>
+		<p>I write a bi-weekly newsletter for curious minds called <a href="https://exploriter.email" title="The Exploriter Newsletter (TEN)" target="_blank">TEN</a> about interesting tools, systems, and ideas to shape and refine how you think and act.</p>
+		<div class="ck">
+			<script src="https://f.convertkit.com/ckjs/ck.5.js"></script>
+			<form action="https://app.convertkit.com/forms/1978150/subscriptions" class="formkit-form" method="post" data-sv-form="1978150" data-uid="9a538e092a" data-format="inline" data-version="5" data-options="{&quot;settings&quot;:{&quot;after_subscribe&quot;:{&quot;action&quot;:&quot;message&quot;,&quot;success_message&quot;:&quot;Please check your email to confirm your subscription.&quot;,&quot;redirect_url&quot;:&quot;&quot;}},&quot;version&quot;:&quot;5&quot;}">
+				<input class="formkit-input" name="email_address" aria-label="Enter your best email..." placeholder="Enter your best email..." required="" type="email">
+				<button data-element="submit" class="formkit-submit">
+					<div class="formkit-spinner"><div>
+					<span>Subscribe</span>
+				</button>
+			</form>
+		</div>
+		<div class="divider"></div>
+		<section>
+			<p class="section-title">More about me (and where I fit in a team)...</p>
+			<h2>What am I good at?</h2>
+			<p>I prefer to manage the <em>how</em> vs. the <em>what</em>. That is to say, <em>context</em> rather than <em>content</em>. To build and support foundations that engender trust and foster curiosity, growth, responsibility, and openness in ways that do not condemn their opposites but instead use techniques to shift from one to the other.</p>
+			<p>I take a human-centered and forgiving approach and aim to minimize ego wherever and whenever possible.</p>
+			<p>As for the work itself, I thrive in the communal and active part of the ideation process—the work that brings clarity to an idea so that it becomes a project—as well as the expansion, attention to detail, and optimization once something is active in the real world.</p>
+			<p>Most work goes through similar inter/intra-looping phases; for simplicity sake, it may look something like...</p>
+			<ol>
+				<li>Ideation (conceptual)</li>
+				<li>Exploration (play)</li>
+				<li>Shaping (à la Basecamp's Shape Up)</li>
+				<li>Assessment (risks and assets)</li>
+				<li>The Work (concept -> reality)</li>
+				<li>Presentation (actualized/contextualized)</li>
+				<li>Improvement (and maintenance)</li>
+			</ol>
+			<p>Using this model, I excel at 2, 3, and 7. I enjoy 1 and 4, and struggle to maintain interest in 5 and 6.</p>
+			<p>However, I have dedicated several years and significant attention toward acquiring above-baseline skills in many areas outside of my immediate interests. As such, I am an excellent translator between the languages of management, strategy, design, development, marketing, sales, and support.</p>
+			<p>I enjoy roles where I can translate complexity into simplicity, contribute to and embody new points of view, demonstrate resourcefulness, and engage my contrarian side in ways that are more dialectic than combative.</p>
+			<p>More practically, this means I am well suited to the role of COO or more strategic and problem-solving roles in Product rather than forward-facing roles in Marketing or Sales.</p>
+			<p>The truth is, I don't care what the role is as long as it affords the opportunities I have mentioned, evokes the pioneering spirit of adventure, and is with a team who are committed to learning, fun, impact, and doing so with candor and grace.</p>
+			<p>In the end, I want to create something that stands the test of time with people who understand that building a team and company is as important—if not more so—than the products or services it provides.</p>
+		</section>
+		<div class="divider"></div>
+	</article>
+	<footer>
+		<p><a href="mailto:watson@hey.com" title="Email me" target="_blank">Email</a> &middot; <a href="https://twitter.com/exploriter" title="@exploriter on Twitter" target="_blank">Twitter</a> &middot; <a href="https://github.com/gigawatson" title="gigawatson on GitHub" target="_blank">GitHub</a>
+	</footer>
+</body>
+</html>
